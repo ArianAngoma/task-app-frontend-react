a8236675ec5ed8721f968bd7450bb1847c6d3846
unknown <arian.angoma@tecsup.edu.pe>
2021-10-16 04:34:57 -0500

    Fourmulario para el Inicio de Sesión

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,5 +1,39 @@
 export const Login = () => {
+    const handleOnChange = () => {
+
+    }
+
     return (
-        <h1>Login</h1>
+        <div className="form-user">
+            <div className="container-form shadow-dark">
+                <h1>Iniciar Sesión</h1>
+
+                <form>
+                    <div className="field-form">
+                        <label htmlFor="email">Email</label>
+                        <input type="email"
+                               id="email"
+                               name="email"
+                               placeholder="Tu email"
+                               onChange={handleOnChange}/>
+                    </div>
+
+                    <div className="field-form">
+                        <label htmlFor="password">Password</label>
+                        <input type="password"
+                               id="password"
+                               name="password"
+                               placeholder="Tu password"
+                               onChange={handleOnChange}/>
+                    </div>
+
+                    <div className="field-form">
+                        <input type="submit"
+                               className="btn btn-primary btn-block"
+                               value="Iniciar Sesión"/>
+                    </div>
+                </form>
+            </div>
+        </div>
     );
 }
