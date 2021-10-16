91efe7a6a77fa5054ced2b4642021094aa0d9e55
unknown <arian.angoma@tecsup.edu.pe>
2021-10-16 17:02:15 -0500

    Componente NewAccount

--- src/components/auth/NewAccount.js
+++ src/components/auth/NewAccount.js
@@ -1,5 +1,86 @@
+import {useState} from 'react';
+import {Link} from 'react-router-dom';
+
 export const NewAccount = () => {
+    /* State para registro de nuevo usuario */
+    const [user, setUser] = useState({
+        name: '',
+        email: '',
+        password: '',
+        confirm: ''
+    });
+    /* Extraer campos de user */
+    const {name, email, password, confirm} = user;
+
+    const handleOnChange = (e) => {
+        setUser({
+            ...user,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    /* Registro de nuevo usuario */
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        /* Validar que no haya campos vacios */
+    };
+
     return (
-        <h1>New Account</h1>
+        <div className="form-user">
+            <div className="container-form shadow-dark">
+                <h1>Obtener una cuenta</h1>
+
+                <form onSubmit={handleSubmit}>
+                    <div className="field-form">
+                        <label htmlFor="name">Nombre</label>
+                        <input type="text"
+                               id="name"
+                               name="name"
+                               placeholder="Tu nombre"
+                               value={name}
+                               onChange={handleOnChange}/>
+                    </div>
+
+                    <div className="field-form">
+                        <label htmlFor="email">Email</label>
+                        <input type="email"
+                               id="email"
+                               name="email"
+                               placeholder="Tu email"
+                               value={email}
+                               onChange={handleOnChange}/>
+                    </div>
+
+                    <div className="field-form">
+                        <label htmlFor="password">Password</label>
+                        <input type="password"
+                               id="password"
+                               name="password"
+                               placeholder="Tu password"
+                               value={password}
+                               onChange={handleOnChange}/>
+                    </div>
+
+                    <div className="field-form">
+                        <label htmlFor="confirm">Confirmar Password</label>
+                        <input type="password"
+                               id="confirm"
+                               name="confirm"
+                               placeholder="Repite tu password"
+                               value={confirm}
+                               onChange={handleOnChange}/>
+                    </div>
+
+                    <div className="field-form">
+                        <input type="submit"
+                               className="btn btn-primary btn-block"
+                               value="Registrarme"/>
+                    </div>
+                </form>
+
+                <Link to="/" className="link-account">Volver a Iniciar Sesión</Link>
+            </div>
+        </div>
     );
 }
