823f7f24ad7be8d44eb59adf8e9f1476e638e666
unknown <arian.angoma@tecsup.edu.pe>
2021-10-16 22:51:45 -0500

    State para el formulario de nuevo proyecto

--- src/components/projects/NewProject.js
+++ src/components/projects/NewProject.js
@@ -1,4 +1,24 @@
+import {useState} from 'react';
+
 export const NewProject = () => {
+    const [project, setProject] = useState({
+        name: ''
+    });
+    const {name} = project;
+
+    /* Leer inputs */
+    const handleChange = (e) => {
+        setProject({
+            ...project,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    /* Enviar formulario */
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
     return (
         <>
             <button type="button"
@@ -6,11 +26,14 @@ export const NewProject = () => {
                 Nuevo Proyecto
             </button>
 
-            <form className="new-project-form">
+            <form className="new-project-form"
+                  onSubmit={handleSubmit}>
                 <input type="text"
                        className="input-text"
                        placeholder="Nombre del proyecto"
-                       name="projectName"/>
+                       name="name"
+                       value={name}
+                       onChange={handleChange}/>
 
                 <input type="submit"
                        className="btn btn-primary btn-block"
