0ca05e52b07d8e9373bf8f6bf5fb9b9b4a8a6419
unknown <arian.angoma@tecsup.edu.pe>
2021-10-20 22:49:37 -0500

    Obtener usuario logueado para toda la app

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,9 +1,11 @@
+import {useEffect} from 'react';
 import {
     BrowserRouter as Router,
     Switch,
     // Route,
     Redirect, Route
 } from 'react-router-dom';
+import {useDispatch, useSelector} from 'react-redux';
 
 /* Importaciones propias */
 import {Login} from '../components/auth/Login';
@@ -11,6 +13,16 @@ import {NewAccount} from '../components/auth/NewAccount';
 import {Projects} from '../components/projects/Projects';
 
 export const AppRouter = () => {
+    const dispatch = useDispatch();
+
+    /* Estado de auth */
+    const {checking, user} = useSelector(state => state.auth);
+
+    /* Validar si tengo un usuario logueado */
+    /*useEffect(() => {
+
+    }, [dispatch]);*/
+
     return (
         <Router>
             <div>
