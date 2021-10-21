86c6b358435c5d67440640fda8aada1732ab7784
unknown <arian.angoma@tecsup.edu.pe>
2021-10-21 03:02:06 -0500

    Mostrar nombre de usuario al estar logueado

--- src/components/layout/Bar.js
+++ src/components/layout/Bar.js
@@ -1,7 +1,12 @@
+import {useSelector} from 'react-redux';
+
 export const Bar = () => {
+    /* Estado de auth */
+    const {user} = useSelector(state => state.auth);
+
     return (
         <header className="app-header">
-            <p className="user-name">Hola <span>Arian Angoma</span></p>
+            <p className="user-name">Hola <span>{user.name}</span></p>
 
             <nav className="nav-main">
                 <button className="btn btn-blank close-session">Cerrar Sesión</button>
